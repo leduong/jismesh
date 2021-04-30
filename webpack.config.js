75b51c9de2fdc9595aa4d1769c3c12b8bd108341
Jeff Au Duong <me@leduong.com>
2021-04-30 16:46:51 +0700

    First commit code

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = (env, argv) => {
+  const prod = (argv.mode === 'production');
+
+  return {
+    entry: './index.js',
+    output: {
+      libraryTarget: 'umd',
+      library: 'jismesh',
+      globalObject: 'this',
+      filename: prod ? 'jismesh.min.js' : 'jismesh.js',
+      path: __dirname + '/dist',
+    },
+    devtool: 'source-map'
+  };
+};
