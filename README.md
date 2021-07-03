cac9ec2f4e40050bc2a8105535ba6be6e6f4f659
Jeff Au Duong <me@leduong.com>
2021-07-03 07:16:23 +0700

    ⭐ update v1.7.21

--- README.md
+++ README.md
@@ -1,33 +1,40 @@
-#jismesh for js
-==========
-<a href="https://www.buymeacoffee.com/leduong" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="32" /></a>
+# jismesh for js
 
+[![NPM version](https://img.shields.io/npm/v/jismesh.svg?style=flat)](https://www.npmjs.com/package/jismesh)
+[![NPM monthly downloads](https://img.shields.io/npm/dm/jismesh.svg?style=flat)](https://npmjs.org/package/jismesh)
+[![NPM total downloads](https://img.shields.io/npm/dt/jismesh.svg?style=flat)](https://npmjs.org/package/jismesh)
+[![](https://data.jsdelivr.com/v1/package/npm/jismesh/badge)](https://www.jsdelivr.com/package/npm/jismesh)
 [![Build Status](https://travis-ci.com/leduong/jismesh.svg?branch=master)](https://travis-ci.com/leduong/jismesh)
+[![Coverage Status](https://coveralls.io/repos/github/leduong/jismesh/badge.svg?branch=master)](https://coveralls.io/github/leduong/jismesh?branch=master)
+[![dependencies Status](https://david-dm.org/leduong/jismesh/status.svg)](https://david-dm.org/leduong/jismesh)
+[![devDependencies Status](https://status.david-dm.org/gh/leduong/jismesh.svg?type=dev)](https://david-dm.org/leduong/jismesh?type=dev)
+
+[![NPM](https://nodei.co/npm/jismesh.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/jismesh/)
+
+<a href="https://www.buymeacoffee.com/leduong" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="32" /></a>
 
 Utilities for the Japanese regional grid system defined in Japanese Industrial Standards (JIS X 0410 地域メッシュ).
 
 python の [jismesh パッケージ](https://pypi.org/project/jismesh/) を JavaScript に移植したものです。
 
-対応地域メッシュコード
-----------------------
+## 対応地域メッシュコード
 
-- 1次(標準地域メッシュ 80km四方): 1
-- 40倍(拡張地域メッシュ 40km四方): 40000
-- 20倍(拡張地域メッシュ 20km四方): 20000
-- 16倍(拡張地域メッシュ 16km四方): 16000
-- 2次(標準地域メッシュ 10km四方): 2
-- 8倍(拡張地域メッシュ 8km四方): 8000
-- 5倍(拡張地域メッシュ 5km四方): 5000
-- 4倍(拡張地域メッシュ 4km四方): 4000
-- 2.5倍(拡張地域メッシュ 2.5km四方): 2500
-- 2倍(拡張地域メッシュ 2km四方): 2000
-- 3次(標準地域メッシュ 1km四方): 3
-- 4次(分割地域メッシュ 500m四方): 4
-- 5次(分割地域メッシュ 250m四方): 5
-- 6次(分割地域メッシュ 125m四方): 6
+- 1 次(標準地域メッシュ 80km 四方): 1
+- 40 倍(拡張地域メッシュ 40km 四方): 40000
+- 20 倍(拡張地域メッシュ 20km 四方): 20000
+- 16 倍(拡張地域メッシュ 16km 四方): 16000
+- 2 次(標準地域メッシュ 10km 四方): 2
+- 8 倍(拡張地域メッシュ 8km 四方): 8000
+- 5 倍(拡張地域メッシュ 5km 四方): 5000
+- 4 倍(拡張地域メッシュ 4km 四方): 4000
+- 2.5 倍(拡張地域メッシュ 2.5km 四方): 2500
+- 2 倍(拡張地域メッシュ 2km 四方): 2000
+- 3 次(標準地域メッシュ 1km 四方): 3
+- 4 次(分割地域メッシュ 500m 四方): 4
+- 5 次(分割地域メッシュ 250m 四方): 5
+- 6 次(分割地域メッシュ 125m 四方): 6
 
-インストール
-------------
+## インストール
 
 ```bash
 npm install jismesh
@@ -41,19 +48,18 @@ npm install jismesh
 
 グローバル変数 `jismesh` が定義されます。
 
-使用例
-------
+## 使用例
 
 ### 緯度経度から地域メッシュコードを求める
 
 メッシュコードに変換する世界測地系緯度経度と変換するメッシュコードの次数を指定します。
 
 ```javascript
-const jismesh = require('jismesh');
+const jismesh = require("jismesh");
 
 // 緯度経度からメッシュコードを求める。
 const meshCode = jismesh.toMeshCode(35.658581, 139.745433, 3);
-console.log(meshCode);  // => 53393599
+console.log(meshCode); // => 53393599
 ```
 
 ### 地域メッシュコードから次数を求める
@@ -61,10 +67,10 @@ console.log(meshCode);  // => 53393599
 メッシュコードからそのメッシュコードの次数を判定します。
 
 ```javascript
-const jismesh = require('jismesh');
+const jismesh = require("jismesh");
 
-const meshLevel = jismesh.toMeshLevel('53393599');
-console.log(meshLevel);  // => 3
+const meshLevel = jismesh.toMeshLevel("53393599");
+console.log(meshLevel); // => 3
 ```
 
 ### 地域メッシュコードから緯度経度を求める
@@ -74,21 +80,25 @@ console.log(meshLevel);  // => 3
 を指定します。
 
 ```javascript
-const jismesh = require('jismesh');
+const jismesh = require("jismesh");
 
 // 南西端の緯度経度を求める。
-const [latSW, lonSW] = jismesh.toMeshPoint('53393599', 0, 0);
-console.log(latSW, lonSW);  // => 35.65833333333333 139.7375
+const [latSW, lonSW] = jismesh.toMeshPoint("53393599", 0, 0);
+console.log(latSW, lonSW); // => 35.65833333333333 139.7375
 
 // 北東端の緯度経度を求める。
-const [latNE, lonNE] = jismesh.toMeshPoint('53393599', 1, 1);
-console.log(latNE, lonNE);  // => 35.666666666666664 139.75
+const [latNE, lonNE] = jismesh.toMeshPoint("53393599", 1, 1);
+console.log(latNE, lonNE); // => 35.666666666666664 139.75
 
 // 中心点の緯度経度を求める。
-const [latC, lonC] = jismesh.toMeshPoint('53393599', 0.5, 0.5);
-console.log(latC, lonC);  // => 35.6625 139.74375
+const [latC, lonC] = jismesh.toMeshPoint("53393599", 0.5, 0.5);
+console.log(latC, lonC); // => 35.6625 139.74375
 
 // 東隣接メッシュの中心点の緯度経度を求める。
-const [latEastNeighborC, lonEastNeighborC] = jismesh.toMeshPoint('53393599', 0.5, 1.5);
-console.log(latEastNeighborC, lonEastNeighborC);  // => 35.6625 139.75625000000002
+const [latEastNeighborC, lonEastNeighborC] = jismesh.toMeshPoint(
+  "53393599",
+  0.5,
+  1.5
+);
+console.log(latEastNeighborC, lonEastNeighborC); // => 35.6625 139.75625000000002
 ```
--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-'use strict';
+"use strict";
 
-const _unitLv1Lat = 40/60;
+const _unitLv1Lat = 40 / 60;
 const _unitLv1Lon = 1;
-const _unitLv2Lat = 5/60;
-const _unitLv2Lon = 7.5/60;
-const _unitLv3Lat = 30/3600;
-const _unitLv3Lon = 45/3600;
-const _unitLv4Lat = 15/3600;
-const _unitLv4Lon = 22.5/3600;
-const _unitLv5Lat = 7.5/3600;
-const _unitLv5Lon = 11.25/3600;
-const _unitLv6Lat = 3.75/3600;
-const _unitLv6Lon = 5.625/3600;
-const _unit40000Lat = 40/120;
-const _unit40000Lon = 1/2;
-const _unit20000Lat = 40/240;
-const _unit20000Lon = 1/4;
-const _unit16000Lat = 40/300;
-const _unit16000Lon = 1/5;
-const _unit8000Lat = 4/60;
-const _unit8000Lon = 6/60;
-const _unit5000Lat = 2.5/60;
-const _unit5000Lon = 3.75/60;
-const _unit4000Lat = 2/60;
-const _unit4000Lon = 3/60;
-const _unit2500Lat = 2.5/120;
-const _unit2500Lon = 3.75/120;
-const _unit2000Lat = 1/60;
-const _unit2000Lon = 1.5/60;
+const _unitLv2Lat = 5 / 60;
+const _unitLv2Lon = 7.5 / 60;
+const _unitLv3Lat = 30 / 3600;
+const _unitLv3Lon = 45 / 3600;
+const _unitLv4Lat = 15 / 3600;
+const _unitLv4Lon = 22.5 / 3600;
+const _unitLv5Lat = 7.5 / 3600;
+const _unitLv5Lon = 11.25 / 3600;
+const _unitLv6Lat = 3.75 / 3600;
+const _unitLv6Lon = 5.625 / 3600;
+const _unit40000Lat = 40 / 120;
+const _unit40000Lon = 1 / 2;
+const _unit20000Lat = 40 / 240;
+const _unit20000Lon = 1 / 4;
+const _unit16000Lat = 40 / 300;
+const _unit16000Lon = 1 / 5;
+const _unit8000Lat = 4 / 60;
+const _unit8000Lon = 6 / 60;
+const _unit5000Lat = 2.5 / 60;
+const _unit5000Lon = 3.75 / 60;
+const _unit4000Lat = 2 / 60;
+const _unit4000Lon = 3 / 60;
+const _unit2500Lat = 2.5 / 120;
+const _unit2500Lon = 3.75 / 120;
+const _unit2000Lat = 1 / 60;
+const _unit2000Lon = 1.5 / 60;
 
 function _toMeshCodeLv1(lat, lon) {
   const y = Math.floor(lat / _unitLv1Lat);
@@ -41,47 +41,47 @@ function _toMeshCodeLv1(lat, lon) {
 function _toMeshCodeLv2(lat, lon) {
   const cellLv1 = _toMeshCodeLv1(lat, lon);
   const y = Math.floor((lat - cellLv1.south) / _unitLv2Lat);
-  const x = Math.floor((lon - cellLv1.west)  / _unitLv2Lon);
+  const x = Math.floor((lon - cellLv1.west) / _unitLv2Lon);
   const code = cellLv1.code + y + x;
   const south = cellLv1.south + y * _unitLv2Lat;
-  const west  = cellLv1.west  + x * _unitLv2Lon;
+  const west = cellLv1.west + x * _unitLv2Lon;
   return { south, west, code };
 }
 
 function _toMeshCodeLv3(lat, lon) {
   const cellLv2 = _toMeshCodeLv2(lat, lon);
   const y = Math.floor((lat - cellLv2.south) / _unitLv3Lat);
-  const x = Math.floor((lon - cellLv2.west)  / _unitLv3Lon);
+  const x = Math.floor((lon - cellLv2.west) / _unitLv3Lon);
   const code = cellLv2.code + y + x;
   const south = cellLv2.south + y * _unitLv3Lat;
-  const west  = cellLv2.west  + x * _unitLv3Lon;
+  const west = cellLv2.west + x * _unitLv3Lon;
   return { south, west, code };
 }
 
 function _toMeshCodeLv4(lat, lon) {
   const cellLv3 = _toMeshCodeLv3(lat, lon);
   const y = Math.floor((lat - cellLv3.south) / _unitLv4Lat);
-  const x = Math.floor((lon - cellLv3.west)  / _unitLv4Lon);
+  const x = Math.floor((lon - cellLv3.west) / _unitLv4Lon);
   const code = cellLv3.code + (2 * y + x + 1);
   const south = cellLv3.south + y * _unitLv4Lat;
-  const west  = cellLv3.west  + x * _unitLv4Lon;
+  const west = cellLv3.west + x * _unitLv4Lon;
   return { south, west, code };
 }
 
 function _toMeshCodeLv5(lat, lon) {
   const cellLv4 = _toMeshCodeLv4(lat, lon);
   const y = Math.floor((lat - cellLv4.south) / _unitLv5Lat);
-  const x = Math.floor((lon - cellLv4.west)  / _unitLv5Lon);
+  const x = Math.floor((lon - cellLv4.west) / _unitLv5Lon);
   const code = cellLv4.code + (2 * y + x + 1);
   const south = cellLv4.south + y * _unitLv5Lat;
-  const west  = cellLv4.west  + x * _unitLv5Lon;
+  const west = cellLv4.west + x * _unitLv5Lon;
   return { south, west, code };
 }
 
 function _toMeshCodeLv6(lat, lon) {
   const cellLv5 = _toMeshCodeLv5(lat, lon);
   const y = Math.floor((lat - cellLv5.south) / _unitLv6Lat);
-  const x = Math.floor((lon - cellLv5.west)  / _unitLv6Lon);
+  const x = Math.floor((lon - cellLv5.west) / _unitLv6Lon);
   const code = cellLv5.code + (2 * y + x + 1);
   return { code };
 }
@@ -89,70 +89,70 @@ function _toMeshCodeLv6(lat, lon) {
 function _toMeshCode40000(lat, lon) {
   const cellLv1 = _toMeshCodeLv1(lat, lon);
   const y = Math.floor((lat - cellLv1.south) / _unit40000Lat);
-  const x = Math.floor((lon - cellLv1.west)  / _unit40000Lon);
+  const x = Math.floor((lon - cellLv1.west) / _unit40000Lon);
   const code = cellLv1.code + (2 * y + x + 1);
   const south = cellLv1.south + y * _unit40000Lat;
-  const west  = cellLv1.west  + x * _unit40000Lon;
+  const west = cellLv1.west + x * _unit40000Lon;
   return { south, west, code };
 }
 
 function _toMeshCode20000(lat, lon) {
   const cell40000 = _toMeshCode40000(lat, lon);
   const y = Math.floor((lat - cell40000.south) / _unit20000Lat);
-  const x = Math.floor((lon - cell40000.west)  / _unit20000Lon);
-  const code = cell40000.code + (2 * y + x + 1) + '5';
+  const x = Math.floor((lon - cell40000.west) / _unit20000Lon);
+  const code = cell40000.code + (2 * y + x + 1) + "5";
   return { code };
 }
 
 function _toMeshCode16000(lat, lon) {
   const cellLv1 = _toMeshCodeLv1(lat, lon);
   const y = Math.floor((lat - cellLv1.south) / _unit16000Lat);
-  const x = Math.floor((lon - cellLv1.west)  / _unit16000Lon);
-  const code = cellLv1.code + (y * 2) + (x * 2) + '7';
+  const x = Math.floor((lon - cellLv1.west) / _unit16000Lon);
+  const code = cellLv1.code + y * 2 + x * 2 + "7";
   return { code };
 }
 
 function _toMeshCode8000(lat, lon) {
   const cellLv1 = _toMeshCodeLv1(lat, lon);
   const y = Math.floor((lat - cellLv1.south) / _unit8000Lat);
-  const x = Math.floor((lon - cellLv1.west)  / _unit8000Lon);
-  const code = cellLv1.code + y + x + '6';
+  const x = Math.floor((lon - cellLv1.west) / _unit8000Lon);
+  const code = cellLv1.code + y + x + "6";
   const south = cellLv1.south + y * _unit8000Lat;
-  const west  = cellLv1.west  + x * _unit8000Lon;
+  const west = cellLv1.west + x * _unit8000Lon;
   return { south, west, code };
 }
 
 function _toMeshCode5000(lat, lon) {
   const cellLv2 = _toMeshCodeLv2(lat, lon);
   const y = Math.floor((lat - cellLv2.south) / _unit5000Lat);
-  const x = Math.floor((lon - cellLv2.west)  / _unit5000Lon);
+  const x = Math.floor((lon - cellLv2.west) / _unit5000Lon);
   const code = cellLv2.code + (2 * y + x + 1);
   const south = cellLv2.south + y * _unit5000Lat;
-  const west  = cellLv2.west  + x * _unit5000Lon;
+  const west = cellLv2.west + x * _unit5000Lon;
   return { south, west, code };
 }
 
 function _toMeshCode4000(lat, lon) {
   const cell8000 = _toMeshCode8000(lat, lon);
   const y = Math.floor((lat - cell8000.south) / _unit4000Lat);
-  const x = Math.floor((lon - cell8000.west)  / _unit4000Lon);
-  const code = cell8000.code + (2 * y + x + 1) + '7';
+  const x = Math.floor((lon - cell8000.west) / _unit4000Lon);
+  const code = cell8000.code + (2 * y + x + 1) + "7";
   return { code };
 }
 
 function _toMeshCode2500(lat, lon) {
   const cell5000 = _toMeshCode5000(lat, lon);
   const y = Math.floor((lat - cell5000.south) / _unit2500Lat);
-  const x = Math.floor((lon - cell5000.west)  / _unit2500Lon);
-  const code = cell5000.code + (2 * y + x + 1) + '6';
+  const x = Math.floor((lon - cell5000.west) / _unit2500Lon);
+  const code = cell5000.code + (2 * y + x + 1) + "6";
   return { code };
 }
 
 function _toMeshCode2000(lat, lon) {
   const cellLv2 = _toMeshCodeLv2(lat, lon);
   const y = Math.floor((lat - cellLv2.south) / _unit2000Lat);
-  const x = Math.floor((lon - cellLv2.west)  / _unit2000Lon);
-  const code = cellLv2.code + (y * 2) + (x * 2) + '5';
+  const x = Math.floor((lon - cellLv2.west) / _unit2000Lon);
+  const code = cellLv2.code + y * 2 + x * 2 + "5";
   return { code };
 }
 
@@ -193,50 +193,62 @@ function toMeshLevel(meshCode) {
   meshCode = String(meshCode);
 
   switch (meshCode.length) {
-  case 4:
-    return 1;
-  case 5:
-    return 40000;
-  case 6:
-    return 2;
-  case 7:
-    switch (meshCode.charAt(6)) {
-    case '1': case '2': case '3': case '4':
-      return 5000;
-    case '6':
-      return 8000;
-    case '5':
-      return 20000;
-    case '7':
-      return 16000;
-    }
-    break;
-  case 8:
-    return 3;
-  case 9:
-    switch (meshCode.charAt(8)) {
-    case '1': case '2': case '3': case '4':
-      return 4;
-    case '5':
-      return 2000;
-    case '6':
-      return 2500;
-    case '7':
-      return 4000;
-    }
-    break;
-  case 10:
-    switch (meshCode.charAt(9)) {
-    case '1': case '2': case '3': case '4':
-      return 5;
-    }
-    break;
-  case 11:
-    switch (meshCode.charAt(10)) {
-    case '1': case '2': case '3': case '4':
-      return 6;
-    }
-    break;
+    case 4:
+      return 1;
+    case 5:
+      return 40000;
+    case 6:
+      return 2;
+    case 7:
+      switch (meshCode.charAt(6)) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+          return 5000;
+        case "6":
+          return 8000;
+        case "5":
+          return 20000;
+        case "7":
+          return 16000;
+      }
+      break;
+    case 8:
+      return 3;
+    case 9:
+      switch (meshCode.charAt(8)) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+          return 4;
+        case "5":
+          return 2000;
+        case "6":
+          return 2500;
+        case "7":
+          return 4000;
+      }
+      break;
+    case 10:
+      switch (meshCode.charAt(9)) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+          return 5;
+      }
+      break;
+    case 11:
+      switch (meshCode.charAt(10)) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+          return 6;
+      }
+      break;
   }
 
   throw new Error(`Unsupported mesh code: ${meshCode}`);
@@ -248,7 +260,7 @@ function _toMeshPointLv1(meshCode) {
   const unitLat = _unitLv1Lat;
   const unitLon = _unitLv1Lon;
   const south = y * unitLat;
-  const west  = x * unitLon + 100;
+  const west = x * unitLon + 100;
   return { south, west, unitLat, unitLon };
 }
 
@@ -259,7 +271,7 @@ function _toMeshPointLv2(meshCode) {
   const unitLat = _unitLv2Lat;
   const unitLon = _unitLv2Lon;
   const south = cellLv1.south + y * unitLat;
-  const west  = cellLv1.west  + x * unitLon;
+  const west = cellLv1.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -270,7 +282,7 @@ function _toMeshPointLv3(meshCode) {
   const unitLat = _unitLv3Lat;
   const unitLon = _unitLv3Lon;
   const south = cellLv2.south + y * unitLat;
-  const west  = cellLv2.west  + x * unitLon;
+  const west = cellLv2.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -282,7 +294,7 @@ function _toMeshPointLv4(meshCode) {
   const unitLat = _unitLv4Lat;
   const unitLon = _unitLv4Lon;
   const south = cellLv3.south + y * unitLat;
-  const west  = cellLv3.west  + x * unitLon;
+  const west = cellLv3.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -294,7 +306,7 @@ function _toMeshPointLv5(meshCode) {
   const unitLat = _unitLv5Lat;
   const unitLon = _unitLv5Lon;
   const south = cellLv4.south + y * unitLat;
-  const west  = cellLv4.west  + x * unitLon;
+  const west = cellLv4.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -306,7 +318,7 @@ function _toMeshPointLv6(meshCode) {
   const unitLat = _unitLv6Lat;
   const unitLon = _unitLv6Lon;
   const south = cellLv5.south + y * unitLat;
-  const west  = cellLv5.west  + x * unitLon;
+  const west = cellLv5.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -318,7 +330,7 @@ function _toMeshPoint40000(meshCode) {
   const unitLat = _unit40000Lat;
   const unitLon = _unit40000Lon;
   const south = cellLv1.south + y * unitLat;
-  const west  = cellLv1.west  + x * unitLon;
+  const west = cellLv1.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -330,7 +342,7 @@ function _toMeshPoint20000(meshCode) {
   const unitLat = _unit20000Lat;
   const unitLon = _unit20000Lon;
   const south = cell40000.south + y * unitLat;
-  const west  = cell40000.west  + x * unitLon;
+  const west = cell40000.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -341,7 +353,7 @@ function _toMeshPoint16000(meshCode) {
   const unitLat = _unit16000Lat;
   const unitLon = _unit16000Lon;
   const south = cellLv1.south + y * unitLat;
-  const west  = cellLv1.west  + x * unitLon;
+  const west = cellLv1.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -352,7 +364,7 @@ function _toMeshPoint8000(meshCode) {
   const unitLat = _unit8000Lat;
   const unitLon = _unit8000Lon;
   const south = cellLv1.south + y * unitLat;
-  const west  = cellLv1.west  + x * unitLon;
+  const west = cellLv1.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -364,7 +376,7 @@ function _toMeshPoint5000(meshCode) {
   const unitLat = _unit5000Lat;
   const unitLon = _unit5000Lon;
   const south = cellLv2.south + y * _unit5000Lat;
-  const west  = cellLv2.west  + x * _unit5000Lon;
+  const west = cellLv2.west + x * _unit5000Lon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -376,7 +388,7 @@ function _toMeshPoint4000(meshCode) {
   const unitLat = _unit4000Lat;
   const unitLon = _unit4000Lon;
   const south = cell8000.south + y * unitLat;
-  const west  = cell8000.west  + x * unitLon;
+  const west = cell8000.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -388,7 +400,7 @@ function _toMeshPoint2500(meshCode) {
   const unitLat = _unit2500Lat;
   const unitLon = _unit2500Lon;
   const south = cell5000.south + y * unitLat;
-  const west  = cell5000.west  + x * unitLon;
+  const west = cell5000.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -399,7 +411,7 @@ function _toMeshPoint2000(meshCode) {
   const unitLat = _unit2000Lat;
   const unitLon = _unit2000Lon;
   const south = cellLv2.south + y * unitLat;
-  const west  = cellLv2.west  + x * unitLon;
+  const west = cellLv2.west + x * unitLon;
   return { south, west, unitLat, unitLon };
 }
 
@@ -425,7 +437,7 @@ function toMeshPoint(meshCode, latMultiplier = 0, lonMultiplier = 0) {
   const level = toMeshLevel(meshCode);
   const { south, west, unitLat, unitLon } = _toMeshPointProcs[level](meshCode);
   const lat = south + latMultiplier * unitLat;
-  const lon = west  + lonMultiplier * unitLon;
+  const lon = west + lonMultiplier * unitLon;
   return [lat, lon];
 }
 
